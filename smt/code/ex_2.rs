7db413c66a346f83f04c8e43102eb99964826671
AdrienChampion <AdrienChampion@users.noreply.github.com>
2022-05-06 09:36:00 +0000

    Deploying to gh-pages from @ AdrienChampion/induction_for_dummies@c5ab63ab6ebb0e867f3ad31343edff71d64f6ca6 🚀

---
+++ smt/code/ex_2.rs
@@ -0,0 +1,12 @@
+vars {
+	x y : int
+}
+
+assert {
+	(x > 7) ∧ (
+		y = 2*x ∨ x = 11
+	)
+}
+
+check_sat!()
+get_model!()
---
+++ smt/code/ex_3_variant.rs
@@ -0,0 +1,11 @@
+vars {
+	x y : int
+}
+
+assert {
+	x > 7,
+	y = 2*x ∨ x = 11,
+}
+
+check_sat!()
+get_model!()
